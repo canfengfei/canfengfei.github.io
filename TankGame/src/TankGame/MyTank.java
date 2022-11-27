b08fa07b8e07555e18f65d023ffb7a499d563834
canfengfei <1437881640@qq.com>
2022-11-27 15:06:34 +0800

    删除文件TankGame

--- TankGame/src/TankGame/MyTank.java
+++
@@ -1,9 +0,0 @@
-package TankGame;
-
-public class MyTank extends Tank{
-
-    public MyTank(int x, int y, int type) {
-        super(x, y,type);
-    }
-
-}
--- TankGame/src/TankGame/test.java
+++
@@ -1,8 +0,0 @@
-package TankGame;
-
-public class test {
-    public static void main(String[] args) {
-        int[] a = new int[3];
-        System.out.println(a);
-    }
-}
